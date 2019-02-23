397515c3d5f6ed90f8ba27e5972ef1986728851f
KMimura <40707315+KMimura@users.noreply.github.com>
2019-02-23 22:44:45 +0900

    finished the second chapter

--- game.go
+++ game.go
@@ -4,17 +4,11 @@ import (
 	"engo.io/engo"
 	"engo.io/engo/common"
 	"engo.io/ecs"
-	"fmt"
 	"image/color"
+	"./systems"
 )
 
 type myScene struct {}
-
-type Player struct {
-	ecs.BasicEntity
-	common.RenderComponent
-	common.SpaceComponent
-}
 
 func (*myScene) Type() string { return "myGame" }
 
@@ -24,34 +18,37 @@ func (*myScene) Preload() {
 }
 
 func (*myScene) Setup(u engo.Updater){
+	engo.Input.RegisterButton("AddPlayer", engo.KeyA)
 	world, _ := u.(*ecs.World)
 	world.AddSystem(&common.RenderSystem{})
-	player := Player{BasicEntity: ecs.NewBasic()}
-	player.SpaceComponent = common.SpaceComponent{
-		Position: engo.Point{10, 10},
-		Width:    10,
-		Height:   10,
-	}
-	texture, err := common.LoadedSprite("pics/greenoctocat.png")
-	if err != nil {
-		fmt.Println("Unable to load texture: " + err.Error())
-	}
-	player.RenderComponent = common.RenderComponent{
-		Drawable: texture,
-		Scale:    engo.Point{1, 1},
-	}
-	for _, system := range world.Systems() {
-		switch sys := system.(type) {
-		case *common.RenderSystem:
-			sys.Add(&player.BasicEntity, &player.RenderComponent, &player.SpaceComponent)
-		}
-	}
+	world.AddSystem(&common.MouseSystem{})
+	world.AddSystem(&systems.PlayerSystem{})
+	// player := Player{BasicEntity: ecs.NewBasic()}
+	// player.SpaceComponent = common.SpaceComponent{
+	// 	Position: engo.Point{10, 10},
+	// 	Width:    30,
+	// 	Height:   30,
+	// }
+	// texture, err := common.LoadedSprite("pics/greenoctocat.png")
+	// if err != nil {
+	// 	fmt.Println("Unable to load texture: " + err.Error())
+	// }
+	// player.RenderComponent = common.RenderComponent{
+	// 	Drawable: texture,
+	// 	Scale:    engo.Point{1, 1},
+	// }
+	// for _, system := range world.Systems() {
+	// 	switch sys := system.(type) {
+	// 	case *common.RenderSystem:
+	// 		sys.Add(&player.BasicEntity, &player.RenderComponent, &player.SpaceComponent)
+	// 	}
+	// }
 }
 
 func main(){
 	opts := engo.RunOptions{
 		Title:"myGame",
-		Width:400,
+		Width:600,
 		Height:400,
 	}
 	engo.Run(opts,&myScene{})
--- systems/systems.go
+++ systems/systems.go
@@ -4,18 +4,65 @@ import (
 	"engo.io/ecs"
 	"fmt"
 	"engo.io/engo"
+	"engo.io/engo/common"
 )
 
-type PlayerSystem struct {}
+type MouseTracker struct {
+    ecs.BasicEntity
+    common.MouseComponent
+}
+
+type Player struct {
+	ecs.BasicEntity
+	common.RenderComponent
+	common.SpaceComponent
+}
+
+type PlayerSystem struct {
+	mouseTracker MouseTracker
+	world *ecs.World
+}
 
 func (*PlayerSystem) Remove(ecs.BasicEntity) {}
 
-func (*PlayerSystem) Update(dt float32) {
+func (pl *PlayerSystem) Update(dt float32) {
 	if engo.Input.Button("AddPlayer").JustPressed()  {
 		fmt.Println("The gamer pressed A")
+		player := Player{BasicEntity: ecs.NewBasic()}
+		player.SpaceComponent = common.SpaceComponent{
+			Position: engo.Point{pl.mouseTracker.MouseX, pl.mouseTracker.MouseY},
+			Width:    30,
+			Height:   30,
+		}
+		texture, err := common.LoadedSprite("pics/greenoctocat.png")
+		if err != nil {
+			fmt.Println("Unable to load texture: " + err.Error())
+		}
+		player.RenderComponent = common.RenderComponent{
+			Drawable: texture,
+			Scale:    engo.Point{0.1, 0.1},
+		}
+		for _, system := range pl.world.Systems() {
+			switch sys := system.(type) {
+			case *common.RenderSystem:
+				sys.Add(&player.BasicEntity, &player.RenderComponent, &player.SpaceComponent)
+			}
+		}
+	
 	}
 }
 
-func (*PlayerSystem) New(*ecs.World){
+func (player *PlayerSystem) New(w *ecs.World){
 	fmt.Println("IT WORKED!!")
+
+	player.world = w
+	player.mouseTracker.BasicEntity = ecs.NewBasic()
+	player.mouseTracker.MouseComponent = common.MouseComponent{Track: true}
+
+	for _, system := range w.Systems() {
+		switch sys := system.(type) {
+		case *common.MouseSystem:
+			sys.Add(&player.mouseTracker.BasicEntity, &player.mouseTracker.MouseComponent, nil, nil)
+		}
+	}
 }
