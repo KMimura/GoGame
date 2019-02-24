2829b5c3eed7231b61ba0e5458e94acae8f07121
KMimura <40707315+KMimura@users.noreply.github.com>
2019-02-24 15:47:56 +0900

    made 'sample' directory

--- sample/game.go
+++ sample/game.go
@@ -13,6 +13,7 @@ type myScene struct {}
 func (*myScene) Type() string { return "myGame" }
 
 func (*myScene) Preload() {
+	engo.Files.Load("pics/greenoctocat.png")
 	common.SetBackground(color.White)
 }
 
@@ -26,6 +27,26 @@ func (*myScene) Setup(u engo.Updater){
 		engo.DefaultVerticalAxis)
 	world.AddSystem(kbs)
 	world.AddSystem(&systems.PlayerSystem{})
+	// player := Player{BasicEntity: ecs.NewBasic()}
+	// player.SpaceComponent = common.SpaceComponent{
+	// 	Position: engo.Point{10, 10},
+	// 	Width:    30,
+	// 	Height:   30,
+	// }
+	// texture, err := common.LoadedSprite("pics/greenoctocat.png")
+	// if err != nil {
+	// 	fmt.Println("Unable to load texture: " + err.Error())
+	// }
+	// player.RenderComponent = common.RenderComponent{
+	// 	Drawable: texture,
+	// 	Scale:    engo.Point{1, 1},
+	// }
+	// for _, system := range world.Systems() {
+	// 	switch sys := system.(type) {
+	// 	case *common.RenderSystem:
+	// 		sys.Add(&player.BasicEntity, &player.RenderComponent, &player.SpaceComponent)
+	// 	}
+	// }
 }
 
 func main(){
