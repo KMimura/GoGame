996e71fab4189e92d697813da48a0cd33a83b2f5
KMimura <40707315+KMimura@users.noreply.github.com>
2019-03-15 00:54:16 +0900

    did a lot of stuff

--- new/game.go
+++ new/game.go
@@ -14,7 +14,8 @@ func (*myScene) Type() string { return "myGame" }
 
 func (*myScene) Preload() {
 	engo.Files.Load("pics/greenoctocat.png")
-	common.SetBackground(color.White)
+	engo.Files.Load("tilemap/tilesheet_grass.png")
+	common.SetBackground(color.RGBA{255, 250, 220, 0})
 }
 
 func (*myScene) Setup(u engo.Updater){
@@ -23,15 +24,17 @@ func (*myScene) Setup(u engo.Updater){
 	engo.Input.RegisterButton("Jump", engo.KeySpace)
 	world, _ := u.(*ecs.World)
 	world.AddSystem(&common.RenderSystem{})
+	world.AddSystem(&systems.TileSystem{})
 	world.AddSystem(&systems.PlayerSystem{})
 }
 
 func main(){
 	opts := engo.RunOptions{
 		Title:"myGame",
-		Width:800,
-		Height:600,
+		Width:400,
+		Height:300,
 		StandardInputs: true,
+		NotResizable:true,
 	}
 	engo.Run(opts,&myScene{})
 }
--- new/systems/systems.go
+++ new/systems/systems.go
@@ -5,11 +5,18 @@ import (
 	"fmt"
 	"engo.io/engo"
 	"engo.io/engo/common"
-	// "image"
-	// "image/color"
+	"reflect"
 )
 
+var Spritesheet *common.Spritesheet
+
 type Player struct {
+	ecs.BasicEntity
+	common.RenderComponent
+	common.SpaceComponent
+}
+
+type Tile struct {
 	ecs.BasicEntity
 	common.RenderComponent
 	common.SpaceComponent
@@ -27,13 +34,31 @@ type PlayerSystem struct {
 	texture *common.Texture
 }
 
+type TileSystem struct {
+	world *ecs.World
+	// x軸座標
+	positionX int
+	// y軸座標
+	positionY int
+	tileEntity *Tile
+	texture *common.Texture
+}
+
 func (*PlayerSystem) Remove(ecs.BasicEntity) {}
+
+func (*TileSystem) Remove(ecs.BasicEntity) {}
+
+func (ts *TileSystem) Update(dt float32) {
+	for _, system := range ts.world.Systems() {
+		fmt.Println(reflect.TypeOf(system))
+	}
+}
 
 func (ps *PlayerSystem) Update(dt float32) {
 
 	if engo.Input.Button("MoveRight").Down()  {
 		if float32(ps.positionX) < engo.WindowWidth() - 10{
-			ps.positionX += 10
+			ps.positionX += 5
 			ps.playerEntity.SpaceComponent = common.SpaceComponent{
 				Position: engo.Point{X:float32(ps.positionX),Y:float32(ps.positionY)},
 				Width:    30,
@@ -43,7 +68,7 @@ func (ps *PlayerSystem) Update(dt float32) {
 	}
 	if engo.Input.Button("MoveLeft").Down()  {
 		if ps.positionX > 10{
-			ps.positionX -= 10
+			ps.positionX -= 5
 			ps.playerEntity.SpaceComponent = common.SpaceComponent{
 				Position: engo.Point{X:float32(ps.positionX),Y:float32(ps.positionY)},
 				Width:    30,
@@ -52,7 +77,9 @@ func (ps *PlayerSystem) Update(dt float32) {
 		}
 	}
 	if engo.Input.Button("Jump").JustPressed() {
-		ps.jumpDuration = 1
+		if ps.jumpDuration == 0 {
+			ps.jumpDuration = 1
+		}
 	}
 
 	if ps.jumpDuration != 0 {
@@ -63,9 +90,9 @@ func (ps *PlayerSystem) Update(dt float32) {
 			Width:    30,
 			Height:   30,
 		}
-		if ps.jumpDuration < 12 {
+		if ps.jumpDuration < 14 {
 			ps.positionY -= 5
-		} else if ps.jumpDuration < 22 {
+		} else if ps.jumpDuration < 26 {
 			ps.positionY += 5
 		} else {
 			ps.jumpDuration = 0
@@ -79,7 +106,7 @@ func (ps *PlayerSystem) New(w *ecs.World){
 	player := Player{BasicEntity: ecs.NewBasic()}
 
 	ps.positionX = int(engo.WindowWidth() / 2)
-	ps.positionY = int(engo.WindowHeight() - 100)
+	ps.positionY = int(engo.WindowHeight() - 88)
 	player.SpaceComponent = common.SpaceComponent{
 		Position: engo.Point{X:float32(ps.positionX),Y:float32(ps.positionY)},
 		Width: 30,
@@ -93,12 +120,52 @@ func (ps *PlayerSystem) New(w *ecs.World){
 		Drawable: texture,
 		Scale: engo.Point{X:0.1, Y:0.1},
 	}
+	player.RenderComponent.SetZIndex(1)
+
 	ps.playerEntity = &player
 	ps.texture = texture
 	for _, system := range ps.world.Systems() {
 		switch sys := system.(type) {
 		case *common.RenderSystem:
 			sys.Add(&player.BasicEntity, &player.RenderComponent, &player.SpaceComponent)
+		}
+	}
+}
+
+func (ts *TileSystem) New(w *ecs.World){
+	ts.world = w
+
+	Spritesheet = common.NewSpritesheetWithBorderFromFile("tilemap/tilesheet_grass.png", 16, 16, 0, 0)
+	Tiles := make([]*Tile, 0)
+	// 地面の描画
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 28; j++ {
+			tile := &Tile{BasicEntity: ecs.NewBasic()}
+            tile.SpaceComponent.Position = engo.Point{
+				X: float32(j * 16),
+				Y: float32(285 - i * 16),
+			}
+			tile.RenderComponent.Drawable = Spritesheet.Cell(17)
+			tile.RenderComponent.SetZIndex(0)
+			Tiles = append(Tiles, tile)
+		}
+	}
+	for j := 0; j < 28; j++ {
+		tile := &Tile{BasicEntity: ecs.NewBasic()}
+		tile.SpaceComponent.Position = engo.Point{
+			X: float32(j * 16),
+			Y: float32(237),
+		}
+		tile.RenderComponent.Drawable = Spritesheet.Cell(1)
+		tile.RenderComponent.SetZIndex(0)
+		Tiles = append(Tiles, tile)
+	}
+	for _, system := range ts.world.Systems() {
+		switch sys := system.(type) {
+		case *common.RenderSystem:
+			for _, v := range Tiles {
+				sys.Add(&v.BasicEntity, &v.RenderComponent, &v.SpaceComponent)
+			}
 		}
 	}
 
